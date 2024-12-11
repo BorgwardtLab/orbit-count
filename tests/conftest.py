628713aa7d1991a548f169b61032fb2714914e4b
Markus Krimmel <krimmel@biochem.mpg.de>
2024-12-11 11:03:51 +0100

    Use an OOP approach instead of global variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,28 @@
+from typing import Any, List
 import pytest
 import subprocess
 import urllib.request
+
+NO_SKIP_OPTION = "--no-skip"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        NO_SKIP_OPTION,
+        action="store_true",
+        default=False,
+        help="also run skipped tests",
+    )
+
+
+def pytest_collection_modifyitems(config, items: List[Any]):
+    if config.getoption(NO_SKIP_OPTION):
+        for test in items:
+            test.own_markers = [
+                marker
+                for marker in test.own_markers
+                if marker.name not in ("skip", "skipif")
+            ]
 
 
 @pytest.fixture(scope="session")
